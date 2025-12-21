d6b072c806b52c4de2327f937ac1f47dccb7425c
vishnu199804 <vishnuramesh1804@gmail.com>
2025-12-21 14:37:06 +0530

    Fix module

--- weathercheck/weathercheck/settings.py
+++ weathercheck/weathercheck/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-u4)-l7dx1hlnx4&zu+0om^gr$wjkgzu^2(y(wc)0=s^f7t2826
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
